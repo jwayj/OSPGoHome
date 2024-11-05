8785262cd92a196bf51ed96befa8828625e24615
Ssomewhat <isc10120@gmail.com>
2024-11-05 21:08:19 +0900

    .

--- static/main.js
+++ static/main.js
@@ -1,11 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleBtn = document.querySelector('.navbar__toogleBtn');
-    const menu = document.querySelector('.navbar__menu');
 
-    toggleBtn.addEventListener('click', function () {
-        menu.classList.toggle('active');
-    });
-});
+
 function openPopup(id) {
     document.getElementById(id).style.display = 'flex';
 }
