e4086ab054f8f76c3a9217dab2e3f8cc899b22fe
jwayj <yeonjoo030701@gmail.com>
2024-11-15 17:13:06 +0900

    Auto stash before merge of "week11" and "origin/week11"

--- database.py
+++ database.py
@@ -19,8 +19,6 @@ class DBhandler:
             "img_path": img_path,
             "status": data['status'],
             "directtransaction": data['directtransaction']
-            
-
         }
         self.db.child("item").child("name").set(item_info)
         print(data,img_path)
@@ -28,8 +26,8 @@ class DBhandler:
     
     def insert_user(self, data, pw):
         user_info ={
-        "id": data['id'], "pw": pw,
-        "name": data['name'] }
+            "id": data['id'], "pw": pw,
+            "name": data['name'] }
         if self.user_duplicate_check(str(data['id'])):
             self.db.child("user").push(user_info)
             print(data) 
@@ -47,4 +45,17 @@ class DBhandler:
                 value = res.val()
                 if value['id'] == id_string:
                     return False
-        return True
+            return True
+    
+    def get_items(self ):
+        items = self.db.child("item").get().val()
+        return items
+    
+    def find_user(self, id_, pw_):
+        users = self.db.child("user").get()
+        target_value=[]
+        for res in users.each():
+            value = res.val()
+            if value['id'] == id_ and value['pw'] == pw_:
+                return True
+        return False
